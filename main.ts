388ea5a3dd288703d9b03116428dc727c359c7ce
Derick Del Rosario <53444782+derickdr@users.noreply.github.com>
2024-10-28 08:58:04 -0700

    project init

--- main.ts
+++ main.ts
@@ -1,1 +1,42 @@
-
+// viking placeholder
+let playerAssets = [img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . d d . . . . . . . 
+    . . . . . . d 1 1 d . . . . . . 
+    . . . . . . c d d c . . . . . . 
+    . . . c c . c b b c . c c . . . 
+    c c f e e f c b b c f e e f c c 
+    c b f f f f b b b b f f f f b c 
+    c b b b b b b b b b b b b b b c 
+    . c f f f f b b b b f f f f c . 
+    . . c b b f b b b b f b b c . . 
+    . . c b c . c b b c . c b c . . 
+    . . . c . . c b b c . . c . . . 
+    . . . . . . . c c . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `]
+let supportAssets = 0
+let combatAssets = 0
+let gameUIAssets = 0
+let protossAssets = 0
+let zergAssets = 0
+let terranAssets = 0
